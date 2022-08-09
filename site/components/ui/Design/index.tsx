f05121d1ca05ebffa245afa8aaf97eb880fd5ac3
AviduDev <avidudev@gmail.com>
2022-08-09 17:11:55 +0530

    till WHY US

--- site/components/ui/Design/index.tsx
+++ site/components/ui/Design/index.tsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import Button from '@components/ui/Button'
+import Link from 'next/link'
+import styles from './Design.module.css'
 
 const Design = () => {
   return (
-    <div>Design</div>
+    <div>
+      <div>
+        <h1 className={styles.des_h1}>HAVE A IDEA?</h1>
+        <div className={styles.des_p}>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi,
+            harum!
+          </p>
+        </div>
+        <Link href="/">
+          <button className={styles.des_btn}>TELL US</button>
+        </Link>
+      </div>
+      <div>
+        <h1 className={styles.des_h1}>HAVE A DESIGN?</h1>
+        <div className={styles.des_p}>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi,
+            harum!
+          </p>
+        </div>
+        <Link href="/">
+          <button className={styles.des_btn}>UPLOAD</button>
+        </Link>
+      </div>
+    </div>
   )
 }
 
