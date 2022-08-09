ac40814c399f1a919232b088988f4e7094c70b08
AviduDev <avidudev@gmail.com>
2022-08-09 14:19:31 +0530

    Hero basic styling added

--- site/components/ui/Hero/Hero.tsx
+++ site/components/ui/Hero/Hero.tsx
@@ -1,12 +1,31 @@
 import React from 'react'
+import styles from './Hero.module.css'
+import Link from 'next/link'
+import cn from 'clsx'
+import Button from '../Button'
 
 const Hero = () => {
   return (
     <div>
       <div>
-        <h1 className='hero_h1'>BUY <br />DESIGN <br />YOUR <br />OWN <br />TEE</h1>
-        <button>SHOP</button>
-        <button>DESIGN</button>
+        <h1 className={styles.hero_h1}>
+          BUY <br />
+          DESIGN <br />
+          YOUR <br />
+          OWN <br />
+          TEE
+        </h1>
+      </div>
+      <div>
+        <Link href="/search">
+          <button className={styles.hero_btn}>SHOP</button>
+        </Link>
+        <Link href="/search">
+          <button className={styles.hero_btn}>DESIGN</button>
+        </Link>
+      </div>
+      <div>
+        <h2 className={styles.hero_h2}>This is where you can buy t-shirts with pre made design or design your own t-shirt with us.</h2>
       </div>
     </div>
   )
