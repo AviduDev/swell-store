2511f743c7b5041723b0832653208b365c002383
AviduDev <avidudev@gmail.com>
2022-08-14 21:56:13 +0530

    contact form added

---
+++ site/components/ui/contactForm/contactForm.tsx
@@ -0,0 +1,28 @@
+import { useForm, ValidationError } from '@formspree/react'
+
+export default function ContactForm() {
+  const [state, handleSubmit] = useForm('process.env.NEXT_PUBLIC_FORM')
+
+  if (state.succeeded) {
+    return <p>Thanks for your submission!</p>
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="email">Email Address</label>
+      <input id="email" type="email" name="email" />
+      <ValidationError prefix="Email" field="email" errors={state.errors} />
+      <br />
+      <label htmlFor="file">Select File</label>
+      <input type="file" name="attachment" />
+      <br />
+      <textarea id="message" name="message" />
+      <ValidationError prefix="Message" field="message" errors={state.errors} />
+      <br />
+      <button type="submit" disabled={state.submitting}>
+        Submit
+      </button>
+      <ValidationError errors={state.errors} />
+    </form>
+  )
+}
--- site/pages/design/index.tsx
+++ site/pages/design/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
+import ContactForm from '@components/ui/contactForm/contactForm'
 import Link from 'next/link'
 import styles from '../../styles/design.module.css'
+
 
 export default function Design() {
   return (
@@ -20,106 +22,20 @@ export default function Design() {
                 natus fugit.
               </p>
             </div>
+
             <div>
-              <form action="" method="get">
-                <label htmlFor="">
-                  Your Name:
-                  <input
-                    type="text"
-                    name="name"
-                    id="name"
-                    placeholder="Your Name"
-                  />
-                </label>
-                <br />
-                <label htmlFor="">
-                  Your E-mail:
-                  <input
-                    type="email"
-                    name="E-mail"
-                    id="email"
-                    placeholder="E-mail"
-                  />
-                </label>
-                <br />
-                <label htmlFor="">
-                  Your File:
-                  <input type="file" name="" id="" />
-                </label>
-                <br />
-                <label htmlFor="">
-                  Additional Info:
-                  <textarea
-                    className={styles.ta}
-                    name="message"
-                    id="message"
-                    cols={30}
-                    rows={10}
-                    placeholder="Add Your Addition Details Here"
-                  ></textarea>
-                </label>
-                <br />
-                <button>
-                  <input type="submit" value="Submit" />
-                </button>
-              </form>
-            </div>
-          </div>
-          <div>
-            <h1 className={styles.des_h1}>HAVE A DESIGN?</h1>
-            <div className={styles.des_p}>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt
-                quasi reiciendis repellat esse minus, molestiae quia minima
-                nostrum fugiat aliquam.
-              </p>
-            </div>
-            <div>
-              <form action="" method="get">
-                <label htmlFor="">
-                  Your Name:
-                  <input
-                    type="text"
-                    name="name"
-                    id="name"
-                    placeholder="Your Name"
-                  />
-                </label>
-                <br />
-                <label htmlFor="">
-                  Your E-mail:
-                  <input
-                    type="email"
-                    name="E-mail"
-                    id="email"
-                    placeholder="E-mail"
-                  />
-                </label>
-                <br />
-                <label htmlFor="">
-                  Your File:
-                  <input type="file" name="" id="" />
-                </label>
-                <br />
-                <label htmlFor="">
-                  Additional Info:
-                  <textarea
-                    className={styles.ta}
-                    name="message"
-                    id="message"
-                    cols={30}
-                    rows={10}
-                    placeholder="Add Your Addition Details Here"
-                  ></textarea>
-                </label>
-                <br />
-                <button>
-                  <input type="submit" value="Submit" />
-                </button>
-              </form>
+              <h1 className={styles.des_h1}>HAVE A DESIGN?</h1>
+              <div className={styles.des_p}>
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt
+                  quasi reiciendis repellat esse minus, molestiae quia minima
+                  nostrum fugiat aliquam.
+                </p>
+              </div>
             </div>
           </div>
         </div>
+        <div><ContactForm /></div>
       </main>
     </div>
   )
