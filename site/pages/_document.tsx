4396fdd7d22434b013ca3a4a1744fe53a55d52e8
AviduDev <avidudev@gmail.com>
2022-08-16 13:59:13 +0530

    Update _document.tsx

--- site/pages/_document.tsx
+++ site/pages/_document.tsx
@@ -1,13 +1,14 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
+import Link from 'next/link'
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-          <link href="https://fonts.googleapis.com/css2?family=Passion+One:wght@400;700;900&family=Sora&display=swap" rel="stylesheet" />
+          <Link rel="preconnect" href="https://fonts.googleapis.com" />
+          <Link rel="preconnect" href="https://fonts.gstatic.com" />
+          <Link href="https://fonts.googleapis.com/css2?family=Passion+One:wght@400;700;900&family=Sora&display=swap" rel="stylesheet" />
         </Head>
         <body className="loading">
           <Main />
