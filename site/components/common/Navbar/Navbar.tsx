5945537588615950dbf6eb437ebccd3fdb847fe2
AviduDev <avidudev@gmail.com>
2022-08-18 15:05:05 +0530

    Nav Done

--- site/components/common/Navbar/Navbar.tsx
+++ site/components/common/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react'
 import Link from 'next/link'
 import s from './Navbar.module.css'
+import styles from './Navbar.module.css'
 import NavbarRoot from './NavbarRoot'
 import { Logo, Container } from '@components/ui'
 import { Searchbar, UserNav } from '@components/common'
@@ -17,30 +18,36 @@ interface NavbarProps {
 const Navbar: FC<NavbarProps> = ({ links }) => (
   <NavbarRoot>
     <Container clean>
-      <div className={s.nav}>
-        <div>
+      <div className={styles.nav_main}>
+        <div className={styles.nav_container}>
+
           <Link href="/">
             <a className={s.logo} aria-label="Logo">
               <Logo />
             </a>
           </Link>
-          <nav className={s.navMenu}>
-            <Link href="/search">
-              <a className={s.link}>SHOP</a>
+
+          <nav className={styles.nav_links}>
+            <Link href="/search" className={styles.nav_link}>
+              <a className={styles.nav_link}>SHOP</a>
             </Link>
-            <Link href="/design">
-              <a>DESIGN</a>
+            <Link href="/design" className={styles.nav_link}>
+              <a className={styles.nav_link}>DESIGN</a>
             </Link>
           </nav>
-        </div>
-        <div>
+
           <UserNav />
         </div>
-        {process.env.COMMERCE_SEARCH_ENABLED && (
-          <div>
-            <Searchbar />
-          </div>
-        )}
+
+        <div className={styles.nav_search}>
+          {' '}
+          {process.env.COMMERCE_SEARCH_ENABLED && (
+            <div>
+              <Searchbar />
+            </div>
+          )}
+        </div>
+
       </div>
     </Container>
   </NavbarRoot>
