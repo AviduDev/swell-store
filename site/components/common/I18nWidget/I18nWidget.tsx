03b6d5fc15b08a960455e1d76eeb995116b0670f
AviduDev <avidudev@gmail.com>
2022-08-18 13:04:38 +0530

    Nav Editing Started

--- site/components/common/I18nWidget/I18nWidget.tsx
+++ site/components/common/I18nWidget/I18nWidget.tsx
@@ -4,7 +4,6 @@ import { FC, useState } from 'react'
 import { useRouter } from 'next/router'
 import s from './I18nWidget.module.css'
 import { Cross, ChevronUp } from '@components/icons'
-import ClickOutside from '@lib/click-outside'
 interface LOCALE_DATA {
   name: string
   img: {
@@ -43,58 +42,7 @@ const I18nWidget: FC = () => {
   const currentLocale = locale || defaultLocale
 
   return (
-    <ClickOutside active={display} onClick={() => setDisplay(false)}>
-      <nav className={s.root}>
-        <div
-          className="flex items-center relative"
-          onClick={() => setDisplay(!display)}
-        >
-          <button className={s.button} aria-label="Language selector">
-            <img
-              width="20"
-              height="20"
-              className="block mr-2 w-5"
-              src={`/${LOCALES_MAP[currentLocale].img.filename}`}
-              alt={LOCALES_MAP[currentLocale].img.alt}
-            />
-            {options && (
-              <span className="cursor-pointer">
-                <ChevronUp className={cn(s.icon, { [s.active]: display })} />
-              </span>
-            )}
-          </button>
-        </div>
-        <div className="absolute top-0 right-0">
-          {options?.length && display ? (
-            <div className={s.dropdownMenu}>
-              <div className="flex flex-row justify-end px-6">
-                <button
-                  onClick={() => setDisplay(false)}
-                  aria-label="Close panel"
-                  className={s.closeButton}
-                >
-                  <Cross className="h-6 w-6" />
-                </button>
-              </div>
-              <ul>
-                {options.map((locale) => (
-                  <li key={locale}>
-                    <Link href={currentPath} locale={locale}>
-                      <a
-                        className={cn(s.item)}
-                        onClick={() => setDisplay(false)}
-                      >
-                        {LOCALES_MAP[locale].name}
-                      </a>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ) : null}
-        </div>
-      </nav>
-    </ClickOutside>
+    <div></div>
   )
 }
 
